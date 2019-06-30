f9a3881c58e6e4a74e8c80c3e914fdb29bfc0d62
djvang <djvang92@gmail.com>
2019-07-01 00:02:50 +0300

    404 page and update styles and ajax

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,29 +1,49 @@
 "use strict";
 
-const API_URL = "https://demo.wp-api.org/wp-json/wp/v2";
+const API_URL = "http://sau.fulltime.com.ua/wp-json/wp/v2";
 
-const app = new Vue({
-    el: "#app",
-    delimiters: ["${", "}"],
-    data: {
-        research: {
-            isFetching: false,
-            page: 1,
-            per_page: 5,
-            data: []
+Vue.component("posts", {
+    props: {
+        type: {
+            type: String,
+            default: "post"
+        },
+        page: {
+            type: Number,
+            default: 1
+        },
+        limit: {
+            type: Number,
+            default: 10
+        },
+        filters: {
+            type: Array,
+            default: () => []
         }
-        // some data
+    },
+    data: function() {
+        return {
+            isFetching: false,
+            posts: []
+        };
     },
     methods: {
-        getResearch: function() {
-            this.research.isFetching = true;
-            this.research.page += 1;
+        next: function() {
+            this.isFetching = true;
 
-            let url = new URL(`${API_URL}/posts`);
+            let type = this.type === "post" ? "posts" : this.type;
+            let url = new URL(`${API_URL}/${type}`);
+            let filters = this.filters.reduce((filters, filter) => {
+                let items = filter.split("=");
+                filters[`filter${items[0]}`] = items[1];
+                return filters;
+            }, {});
+
             let params = {
                 _embed: 1,
-                page: this.research.page,
-                per_page: this.research.per_page
+                page: this.page,
+                per_page: this.limit,
+                ...filters
             };
             url.search = new URLSearchParams(params);
 
@@ -33,12 +53,12 @@ const app = new Vue({
                     if (json.message) {
                         throw new Error(json.message);
                     }
-                    this.research.data.push(...json);
-                    this.research.isFetching = false;
+                    this.posts.push(...json);
+                    this.isFetching = false;
+                    this.page += 1;
                 })
                 .catch(error => {
-                    this.research.isFetching = false;
-                    console.log(error.message);
+                    this.isFetching = false;
                     UIkit.notification({
                         message: error.message,
                         status: "danger",
@@ -47,7 +67,6 @@ const app = new Vue({
                     });
                 });
         }
-        // some get data
     },
     filters: {
         formatDate: function(value) {
@@ -61,4 +80,65 @@ const app = new Vue({
             return new Date(value).toLocaleDateString("uk-UA", options);
         }
     }
+});
+
+const app = new Vue({
+    el: "#app",
+    delimiters: ["${", "}"]
+    // data: {
+    //     research: {
+    //         isFetching: false,
+    //         page: 1,
+    //         per_page: 5,
+    //         data: []
+    //     }
+    //     // some data
+    // },
+    // methods: {
+    //     getResearch: function() {
+    //         this.research.isFetching = true;
+    //         this.research.page += 1;
+
+    //         let url = new URL(`${API_URL}/posts`);
+    //         let params = {
+    //             _embed: 1,
+    //             page: this.research.page,
+    //             per_page: this.research.per_page
+    //         };
+    //         url.search = new URLSearchParams(params);
+
+    //         fetch(url)
+    //             .then(response => response.json())
+    //             .then(json => {
+    //                 if (json.message) {
+    //                     throw new Error(json.message);
+    //                 }
+    //                 this.research.data.push(...json);
+    //                 this.research.isFetching = false;
+    //             })
+    //             .catch(error => {
+    //                 this.research.isFetching = false;
+    //                 console.log(error.message);
+    //                 UIkit.notification({
+    //                     message: error.message,
+    //                     status: "danger",
+    //                     pos: "top-right",
+    //                     timeout: 3000
+    //                 });
+    //             });
+    //     }
+    //     // some get data
+    // },
+    // filters: {
+    //     formatDate: function(value) {
+    //         if (!value) return "";
+    //         value = value.toString();
+    //         const options = {
+    //             year: "numeric",
+    //             month: "long",
+    //             day: "numeric"
+    //         };
+    //         return new Date(value).toLocaleDateString("uk-UA", options);
+    //     }
+    // }
 });
